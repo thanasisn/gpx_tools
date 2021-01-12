33ed9cc4ab099f81e0bbc43e198181dad57510f7
natsis <11601717+natsis@users.noreply.github.com>
2021-01-12 12:12:31 +0200

    Auto commit 2021-01-12_12:12:31

---
+++ gpx_to_csv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## created on 2015-09-10
+
+
+folderIN="$1"
+folderOUT="$2"
+
+if [[ -d $folderIN ]]; then
+    echo "valid folder"
+else
+    echo "NOT VALID FOLDER!"
+    exit
+fi
+
+if [[ -d $folderOUT ]]; then
+    echo "valid folder"
+else
+    echo "NOT VALID FOLDER!"
+    exit
+fi
+
+find "$folderIN" -type f -iname "*.gpx" | sed  's,'"$folderIN"',,' | while read line;do
+    echo "${folderIN}${line}"
+    mkdir -p "$(dirname "${folderOUT}${line}")"
+
+    gpsbabel -w -r -t -i gpx -f - -o unicsv -F - <"${folderIN}${line}" >"${folderOUT}${line%.*}.csv"
+done
+
+exit 0
