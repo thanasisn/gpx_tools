706ba18ef654aca7c4c45dfdd0e64b54ab5ed833
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-02-04 15:13:01 +0200

    Auto commit

--- gpx_plot_compare_files.R
+++ gpx_plot_compare_files.R
@@ -10,21 +10,18 @@ args <- commandArgs(trailingOnly=TRUE)
 ##TEST
 # args <- c("2.gpx", "1.gpx")
 
-## Use a system command to also open the files
+## Use a system command to run with files
 system_command <- "viking"
 
-## TODO a metho to plot waypoints
-## cases with only waypoints
-## cases with only tracks
 
 
-## test if there is at least one argument: if not, return an error
+####  Check  args  #############################################################
+
+## number of args
 if (length(args)==0) {
     stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
-
-
-####  Check  args  ####
+## filter existing and unique files
 files <- c()
 for (aa in args) {
     if (file.exists(aa)) {
@@ -32,9 +29,9 @@ for (aa in args) {
     }
 }
 files <- unique(files)
-
+## check files we got
 if (length( files ) == 0) {
-    cat(paste("No file exist\n"))
+    stop(paste("No files found. ARGS:", args),"\n")
 } else {
     cat(paste("Found",length(files),"files\n"))
     cat(paste(files,"\n"))
@@ -42,7 +39,8 @@ if (length( files ) == 0) {
 
 
 
-####  GPX files analysis  ####
+####  GPX files analysis  ######################################################
+
 library(sf)
 library(data.table)
 
@@ -79,6 +77,18 @@ cat(paste("Waypoints:  ", nrow(gather_wpt)),"\n")
 cat(paste("Trackpoints:", nrow(gather)    ),"\n")
 
 
+if (nrow(gather) > 0 ) {
+    HAVE_TRACKS <- TRUE
+} else {
+    HAVE_TRACKS <- FALSE
+}
+
+if (nrow(gather_wpt) > 0 ) {
+    HAVE_WPT <- TRUE
+} else {
+    HAVE_WPT <- FALSE
+}
+
 if (all(is.na(gather$time))) {
     NO_TIMES <- TRUE
 } else {
@@ -87,12 +97,12 @@ if (all(is.na(gather$time))) {
 
 
 
-####  Plots  ####
+####  Plots  ###################################################################
 
 ##TODO
-## Add map?
+## Add map? what if we are offline?
 ## Plot speed
-## Plot elevation
+## Plot elevation combine with time plot?
 
 
 X11()
@@ -101,29 +111,60 @@ if (!NO_TIMES) {
     layout(matrix(c(1,1,2), 3, 1, byrow = TRUE))
 }
 
-## plot track points
+xlim <- range( gather$X, gather_wpt$X, na.rm = T )
+ylim <- range( gather$Y, gather_wpt$Y, na.rm = T )
+
+## start area plot
 par(mar=c(2.5,2,0.3,0.3))
-plot(1, type="n", xlab="", ylab="", xlim=range(gather$X), ylim=range(gather$Y),cex.axis = 0.7)
-cols <- 2
-fnam <- c()
-lcol <- c()
-for (af in unique(gather$file)) {
-    temp <- gather[file==af]
-    lines(temp$X, temp$Y, col=scales::alpha(cols , 0.5), lwd = 3)
+plot(1, type="n",
+     xlab = "", ylab = "",
+     xlim = xlim, ylim = ylim,
+     cex.axis = 0.7)
 
-    lcol <- c(lcol, cols)
-    fnam <- c(fnam, basename(af))
+## plot lines
+if (HAVE_TRACKS) {
+    cols <- 2
+    fnam <- c()
+    lcol <- c()
 
-    cols  <- cols + 1
+    for (af in unique(gather$file)) {
+        temp <- gather[file==af]
+        lines(temp$X, temp$Y, col=scales::alpha(cols , 0.5), lwd = 3)
+
+        lcol <- c(lcol, cols)
+        fnam <- c(fnam, basename(af))
+
+        cols  <- cols + 1
+    }
+    par(xpd=TRUE)
+    legend("topleft", legend = fnam,lty = 1, col = lcol,
+           cex = 0.7, ncol = 2, bty="n")
 }
-par(xpd=TRUE)
-# legend(min(gather$X),max(gather$Y), legend = fnam, pch=19, col = lcol,
-#        cex = 0.6, ncol = 2, bty="n")
-legend("topleft", legend = fnam, pch=19, col = lcol,
-       cex = 0.7, ncol = 2, bty="n")
+
+if (HAVE_WPT) {
+    cols <- 2
+    fnam <- c()
+    lcol <- c()
+
+    for (af in unique(gather_wpt$file)) {
+        temp <- gather_wpt[file==af]
+        points(temp$X, temp$Y, col = scales::alpha(cols , 0.5), pch = 19)
+        text(  temp$X, temp$Y, labels=temp$name, cex= 0.7, pos = 4)
+
+        lcol <- c(lcol, cols)
+        fnam <- c(fnam, basename(af))
+
+        cols  <- cols + 1
+    }
+    par(xpd=TRUE)
+    legend("bottomleft", legend = fnam, pch=19, col = lcol,
+           cex = 0.7, ncol = 2, bty="n")
+}
 
 
 
+
+## start time plot
 if (!NO_TIMES) {
 
     ##TODO
@@ -133,7 +174,10 @@ if (!NO_TIMES) {
     cols <- 2
     comp <- 0.1
 
-    plot(1, xaxt="n",yaxt="n", type="n", xlab="", ylab="", xlim=range(gather$time,na.rm = T), ylim=c(0,cols+length(unique(gather$file))))
+    plot(1, type="n",
+         xaxt = "n", yaxt = "n",
+         xlab = "" , ylab = "" ,
+         xlim = range(gather$time,na.rm = T), ylim = c(0,cols+length(unique(gather$file))))
 
     fnam <- c()
     lcol <- c()
@@ -158,17 +202,13 @@ if (!NO_TIMES) {
 
 
 
-
-
-
-
-####  Call a system program  ####
+####  Call a system program  ###################################################
 try(
     system(paste(system_command,
                  paste(shQuote(path.expand(files), "cmd"),collapse = " "),
                  " &"))
 )
-
+## wait for input
 locator(1)
 
 
